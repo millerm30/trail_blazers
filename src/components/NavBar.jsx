b9f508ed1aaf78e642f0293bee4a77d5a55358d4
Michael Miller <millergm30@gmail.com>
2023-02-26 20:21:21 -0330

    hamburger menu color set for mobile

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -16,7 +16,7 @@ const NavBar = () => {
                 className="h-5 w-5"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke="currentColor"
+                stroke="black"
               >
                 <path
                   strokeLinecap="round"
