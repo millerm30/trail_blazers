f5f1216f51bb4d88b18338a03a029a300e9a634b
Michael Miller <millergm30@gmail.com>
2023-02-27 23:41:32 -0330

    smooth scroll to top setup. Props for page names.

---
+++ src/components/ScrollToTop.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react'
+
+export default function ScrollTop() {
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 100) {
+      setVisible(true);
+    } else if (scrolled <= 300) {
+      setVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisible);
+    return () => {
+      window.removeEventListener('scroll', toggleVisible);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+
+  return (
+    <div className="scroll-to-top">
+      {visible && (
+        <div
+          onClick={scrollToTop}
+          className="fixed right-0 bottom-24 bg-[#FB8500] p-4 rounded-l-lg text-white font-bold cursor-pointer"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M5 10l7-7m0 0l7 7m-7-7v18"
+            />
+          </svg>
+        </div>
+      )}
+    </div>
+  );
+}
