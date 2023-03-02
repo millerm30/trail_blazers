6b6851aaa6dd36988a90d0d14bca7e0997d14335
Michael Miller <millergm30@gmail.com>
2023-03-01 20:53:04 -0330

    small change to trail maps

--- src/components/TrailComponent.jsx
+++ src/components/TrailComponent.jsx
@@ -10,7 +10,7 @@ const TrailComponent = ({ trailImage, trailName, trailDescription, trailLength, 
           className="max-w-sm mx-auto"
         />
       </div>
-      <div className="mx-auto md:mx-0 lg:mx-0 bg-white p-6 rounded-lg border-2 border-gray-300">
+      <div className="mx-auto md:mx-0 lg:mx-0 bg-white p-6 rounded-lg border-2 border-gray-300 max-w-md">
         <h2 className="text-3xl font-bold mb-2">{trailName}</h2>
         <p className="mb-2">{trailDescription}</p>
         <p className="mb-2"><span className="font-bold">Length: </span>{trailLength}</p>
